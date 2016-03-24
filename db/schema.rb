7194a0833bd40fed9007417a0531a94f41e92d71
fullerenedream <fullerenedream@gmail.com>
2016-03-24 21:00:59 +0000

    adding city column to spaces table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324192044) do
+ActiveRecord::Schema.define(version: 20160324205731) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160324192044) do
     t.text     "description"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "city"
   end
 
   add_index "spaces", ["user_id"], name: "index_spaces_on_user_id"
