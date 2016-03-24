0b19aa05d25e3b99318518d04abe2bfa6b9196f0
cmdshepard <webdesigndynamics@gmail.com>
2016-03-24 15:58:27 -0700

    Fixed bugs with image sizing

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-
+/*
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
     var width = $('body').css('width')
     $('.jumbotron').css('width', width)
-
+*/
 });
