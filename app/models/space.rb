367ab8afa7ce91593879e45cc4ee9aa1d1b768f8
fullerenedream <fullerenedream@gmail.com>
2016-03-25 22:10:50 +0000

    rounding sq m and fixing validations

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,13 +3,21 @@ class Space < ActiveRecord::Base
   belongs_to :user
 
   validates :square_meters, presence: true, numericality: {only_float: true, greater_than_or_equal_to: 0.5}
-  validates :outdoors, presence: true
-  validates :unit_number, presence: true
+  validates :outdoors, inclusion: { in: [true, false] }
   validates :street_address, presence: true
   validates :province, presence: true
   validates :postal_code, presence: true
   validates :country, presence: true
   validates :city, presence: true
 
+  after_create :round_square_meters
+
+
+  private
+
+  def round_square_meters
+    self.square_meters = self.square_meters.round(1)
+    self.save
+  end
 
 end
